3a96450c2a0d54785dcc7c717823fd8c749627b4
korol4ik4 <korol4ik@tutanota.com>
2022-08-11 22:00:04 +0200

    assistent V0.0001

--- assistent.py
+++ assistent.py
@@ -37,6 +37,7 @@ class Assistant(object):
                 # есть что- то к исполнению
                 for class_name, keywords, message in new_tasks:
                     if class_name in self.all_plugins:
+                        message.keyword = keywords
                         exe_func = self.all_plugins[class_name].on_command
                         exe_func(message)
                     else:
