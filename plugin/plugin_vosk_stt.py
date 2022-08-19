d51ea199dfa6d2a7d5a0ef12e38ba535fd259a0d
korol4ik4 <korol4ik@tutanota.com>
2022-08-19 13:24:13 +0200

    plugin vosk. add recognize from file

--- plugin/plugin_vosk_stt.py
+++ plugin/plugin_vosk_stt.py
@@ -3,7 +3,7 @@
 from plugin import Plugin
 from model.stt_vosk import SpeechToText
 from message import Message
-# import logging
+import logging
 
 
 class SpeechToTextPlugin(Plugin):
@@ -12,6 +12,7 @@ class SpeechToTextPlugin(Plugin):
     def __init__(self):
         super(SpeechToTextPlugin, self).__init__()
         # Инициализация и запуск распознавания голоса
+        self.logger = logging.getLogger("Assistant.Plugin.VoskSTTInput")
         self.stt = SpeechToText()
         self.stt.bind(self.voice_input)
         self.talk_to('INPUT', keyword="*")  # подписать plugin to_name на события от текущего plugin self.name
@@ -42,15 +43,21 @@ class SpeechToTextPlugin(Plugin):
             #self.stt.stop()
             self.stt.mute_on()
             self.say(msg)
-
         elif "mute_off" in cmd:
             #self.stt.start()
             self.stt.mute_off()
-        elif "rec_vaw" in cmd:
+
+        elif "record_vaw" in cmd:
             if msg.file_name:
                 self.stt.rec_wav(msg.file_name)
-        elif "rec_vaw_stop" in cmd:
+        elif "record_vaw_stop" in cmd:
             self.stt.rec_wav_stop()
+
+        elif "recognize_from_file" in cmd:
+            if msg.file_name:
+                self.stt.from_file(msg.file_name, msg.sender)
 
     def close(self):  # выйти из бесконечного цикла (внутри модуля stt_vosk) перед закрытием программы / плагина
         self.stt.stop()
+        self.logger.info("Выход")
+
