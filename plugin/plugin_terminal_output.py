05b0b7b84d810e0dc3c2dba9059d486b5e33c8ea
korol4ik4 <korol4ik@tutanota.com>
2022-10-04 19:57:45 +0200

    +logger -print

--- plugin/plugin_terminal_output.py
+++ plugin/plugin_terminal_output.py
@@ -2,13 +2,8 @@
 # Базовый класс плагинов, переменные класса event и task (общий для всех) позволяют плагинам обмениваться сообщениями
 # name - необходимое имя плагина по которому происходит обращение
 # предложение использовать прописные(заглавные) из имени файла, без приставки plugin_
-from message import Message
 from plugin import Plugin
 import logging
-from threading import Thread
-import pyautogui
-
-
 
 
 class TerminalOutputPlugin(Plugin):
@@ -18,10 +13,11 @@ class TerminalOutputPlugin(Plugin):
     def __init__(self):
         super(TerminalOutputPlugin, self).__init__()
         self.logger = logging.getLogger("Assistant.Plugin.TerminalOutput")
-        self.listen_from('TERMINAL_INPUT', keyword="") # подписать plugin to_name на события от текущего plugin self.name
+        # подписать plugin to_name на события от текущего plugin self.name
+        self.listen_from('TERMINAL_INPUT', keyword="")
         # self.listen_from(self, from_name, keyword="*")  # подписаться на события от plugin from_name
 
     # Если plugin подписан на события, то при его возникновении Ассистент запускает эту функцию
 
     def exe_command(self, message):
-        print("EXE COMMAND TerminalOutput : ", message)
+        self.logger.debug(" : %s", message)
