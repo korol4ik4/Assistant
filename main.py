2557d6196adeef619ed53cd5c47577d6f586714d
korol4ik4 <korol4ik@tutanota.com>
2023-03-19 17:49:32 +0100

    Возможность запуска ТерминалАссистента или Ассистента аналогичным образом.

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ my_assistent.init_plugins()
 my_assistent.loop_start()
 
 my_assistent.loop_input()
+'''
+my_assistent =  Assistant() # Assistant()
+my_assistent.init_plugins()
+my_assistent.loop_start()
+'''
