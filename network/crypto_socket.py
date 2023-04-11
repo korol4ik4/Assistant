15774dae1ccafcdcc47b2ebf5fe624b3378b35a5
korol4ik4 <korol4ik@tutanota.com>
2023-04-11 20:31:07 +0200

    clean server control

--- network/crypto_socket.py
+++ network/crypto_socket.py
@@ -86,8 +86,10 @@ class CryptoSocket(socket):
             connect.sendall(enc_serv_msg,buffer_size)
             sleep(0.01)# иначе сольются данные, пока так
             connect.sendall(enc_data,buffer_size)
+            return True
         except BaseException as e:
-            print(e)
+            print("can't __send__", e)
+            return False
 
             #raise Exception("fail to send")
 
